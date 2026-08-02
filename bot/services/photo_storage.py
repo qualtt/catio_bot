8b299e7b7d031b38cca8fd92cb04aade64649850
qualtt <qualtt.io@gmail.com>
2026-08-02 13:38:25 +0300

    Completely patch botocore to use SOCKSProxyManager for socks5 proxies

--- bot/services/photo_storage.py
+++ bot/services/photo_storage.py
@@ -41,9 +41,50 @@ def _require_bucket() -> str:
     return config.S3_BUCKET
 
 
+import botocore.httpsession
+
+original_get_proxy_manager = botocore.httpsession.URLLib3Session._get_proxy_manager
+
+
+def _patched_get_proxy_manager(self, proxy_url):
+    if proxy_url not in self._proxy_managers:
+        proxy_headers = self._proxy_config.proxy_headers_for(proxy_url)
+        proxy_ssl_context = self._setup_proxy_ssl_context(proxy_url)
+        proxy_manager_kwargs = self._get_pool_manager_kwargs(proxy_headers=proxy_headers)
+        proxy_manager_kwargs.update(self._proxies_kwargs(proxy_ssl_context=proxy_ssl_context))
+
+        if proxy_url.startswith(("socks5:", "socks5h:")):
+            from urllib3.contrib.socks import SOCKSProxyManager
+
+            proxy_manager = SOCKSProxyManager(proxy_url, **proxy_manager_kwargs)
+        else:
+            proxy_manager = botocore.httpsession.proxy_from_url(proxy_url, **proxy_manager_kwargs)
+
+        proxy_manager.pool_classes_by_scheme = self._pool_classes_by_scheme
+        self._proxy_managers[proxy_url] = proxy_manager
+
+    return self._proxy_managers[proxy_url]
+
+
+botocore.httpsession.URLLib3Session._get_proxy_manager = _patched_get_proxy_manager
+
+original_fix_proxy_url = botocore.httpsession.ProxyConfiguration._fix_proxy_url
+
+
+def _patched_fix_proxy_url(self, proxy_url):
+    if proxy_url.startswith(("socks5:", "socks5h:")):
+        return proxy_url
+    return original_fix_proxy_url(self, proxy_url)
+
+
+botocore.httpsession.ProxyConfiguration._fix_proxy_url = _patched_fix_proxy_url
+
+
 def _s3_client():
+    proxy = config.TELEGRAM_PROXY_URL or config.GEMINI_PROXY_URL
     client_config = Config(
         s3={"addressing_style": "path" if config.S3_FORCE_PATH_STYLE else "auto"},
+        proxies={"http": proxy, "https": proxy} if proxy else None,
     )
     kwargs = {
         "region_name": config.S3_REGION,
