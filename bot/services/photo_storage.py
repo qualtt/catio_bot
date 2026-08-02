27b984859d29fc58d2c23fce2a54aeb7635ed5a6
qualtt <qualtt.io@gmail.com>
2026-08-02 13:34:07 +0300

    Revert S3 proxy support

--- bot/services/photo_storage.py
+++ bot/services/photo_storage.py
@@ -41,25 +41,9 @@ def _require_bucket() -> str:
     return config.S3_BUCKET
 
 
-import botocore.httpsession
-
-original_fix_proxy_url = botocore.httpsession.ProxyConfiguration._fix_proxy_url
-
-
-def _patched_fix_proxy_url(self, proxy_url):
-    if proxy_url.startswith(("socks5:", "socks5h:")):
-        return proxy_url
-    return original_fix_proxy_url(self, proxy_url)
-
-
-botocore.httpsession.ProxyConfiguration._fix_proxy_url = _patched_fix_proxy_url
-
-
 def _s3_client():
-    proxy = config.TELEGRAM_PROXY_URL or config.GEMINI_PROXY_URL
     client_config = Config(
         s3={"addressing_style": "path" if config.S3_FORCE_PATH_STYLE else "auto"},
-        proxies={"http": proxy, "https": proxy} if proxy else None,
     )
     kwargs = {
         "region_name": config.S3_REGION,
